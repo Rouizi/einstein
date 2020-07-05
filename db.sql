ab5bc08f5ce1bd52ed71ceb2f2de05dda9098736
rouizi <cinorouizi@gmail.com>
2020-07-05 13:20:42 +0200

    saving data from the exercise table in db.sql file

--- db.sql
+++ db.sql
@@ -16,250 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `auth_group`
+-- Table structure for table `core_exercise`
 --
 
-DROP TABLE IF EXISTS `auth_group`;
+DROP TABLE IF EXISTS `core_exercise`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `auth_group` (
+CREATE TABLE `core_exercise` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(150) NOT NULL,
+  `link` varchar(200) NOT NULL,
+  `wihda_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `core_exercise_wihda_id_d84681e7_fk_core_wihda_id` (`wihda_id`),
+  CONSTRAINT `core_exercise_wihda_id_d84681e7_fk_core_wihda_id` FOREIGN KEY (`wihda_id`) REFERENCES `core_wihda` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `auth_group`
+-- Dumping data for table `core_exercise`
 --
 
-LOCK TABLES `auth_group` WRITE;
-/*!40000 ALTER TABLE `auth_group` DISABLE KEYS */;
-/*!40000 ALTER TABLE `auth_group` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `auth_group_permissions`
---
-
-DROP TABLE IF EXISTS `auth_group_permissions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `auth_group_permissions` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `group_id` int NOT NULL,
-  `permission_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `auth_group_permissions_group_id_permission_id_0cd325b0_uniq` (`group_id`,`permission_id`),
-  KEY `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` (`permission_id`),
-  CONSTRAINT `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
-  CONSTRAINT `auth_group_permissions_group_id_b120cbf9_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `auth_group_permissions`
---
-
-LOCK TABLES `auth_group_permissions` WRITE;
-/*!40000 ALTER TABLE `auth_group_permissions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `auth_group_permissions` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `auth_permission`
---
-
-DROP TABLE IF EXISTS `auth_permission`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `auth_permission` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `content_type_id` int NOT NULL,
-  `codename` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `auth_permission_content_type_id_codename_01ab375a_uniq` (`content_type_id`,`codename`),
-  CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `auth_permission`
---
-
-LOCK TABLES `auth_permission` WRITE;
-/*!40000 ALTER TABLE `auth_permission` DISABLE KEYS */;
-INSERT INTO `auth_permission` VALUES (1,'Can add log entry',1,'add_logentry'),(2,'Can change log entry',1,'change_logentry'),(3,'Can delete log entry',1,'delete_logentry'),(4,'Can view log entry',1,'view_logentry'),(5,'Can add permission',2,'add_permission'),(6,'Can change permission',2,'change_permission'),(7,'Can delete permission',2,'delete_permission'),(8,'Can view permission',2,'view_permission'),(9,'Can add group',3,'add_group'),(10,'Can change group',3,'change_group'),(11,'Can delete group',3,'delete_group'),(12,'Can view group',3,'view_group'),(13,'Can add content type',4,'add_contenttype'),(14,'Can change content type',4,'change_contenttype'),(15,'Can delete content type',4,'delete_contenttype'),(16,'Can view content type',4,'view_contenttype'),(17,'Can add session',5,'add_session'),(18,'Can change session',5,'change_session'),(19,'Can delete session',5,'delete_session'),(20,'Can view session',5,'view_session'),(21,'Can add school',6,'add_school'),(22,'Can change school',6,'change_school'),(23,'Can delete school',6,'delete_school'),(24,'Can view school',6,'view_school'),(25,'Can add wihda',7,'add_wihda'),(26,'Can change wihda',7,'change_wihda'),(27,'Can delete wihda',7,'delete_wihda'),(28,'Can view wihda',7,'view_wihda'),(29,'Can add user',8,'add_user'),(30,'Can change user',8,'change_user'),(31,'Can delete user',8,'delete_user'),(32,'Can view user',8,'view_user');
-/*!40000 ALTER TABLE `auth_permission` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `core_school`
---
-
-DROP TABLE IF EXISTS `core_school`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `core_school` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `school_year` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `core_school`
---
-
-LOCK TABLES `core_school` WRITE;
-/*!40000 ALTER TABLE `core_school` DISABLE KEYS */;
-INSERT INTO `core_school` VALUES (1,'الأولى ثانوي'),(2,'الثانية ثانوي'),(3,'الثالثة ثانوي');
-/*!40000 ALTER TABLE `core_school` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `core_wihda`
---
-
-DROP TABLE IF EXISTS `core_wihda`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `core_wihda` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(70) NOT NULL,
-  `school_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `core_wihda_school_id_4081f7c2_fk_core_school_id` (`school_id`),
-  CONSTRAINT `core_wihda_school_id_4081f7c2_fk_core_school_id` FOREIGN KEY (`school_id`) REFERENCES `core_school` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `core_wihda`
---
-
-LOCK TABLES `core_wihda` WRITE;
-/*!40000 ALTER TABLE `core_wihda` DISABLE KEYS */;
-INSERT INTO `core_wihda` VALUES (1,'الوحدة 1',1),(2,'الوحدة 1',2),(3,'الوحدة 1: المتابعة الزمنية لتطور تحول كيميائي في وسط مائي',3),(4,'الوحدة 2',1),(5,'الوحدة 2',2),(6,'الوحدة 2: دراسة تحولات نووية',3),(7,'الوحدة 3',1),(8,'الوحدة 3',2),(9,'الوحدة 3: دراسة الظواهر الكهربائية',3),(10,'الوحدة 4',1),(11,'الوحدة 4',2),(12,'الوحدة 4: تطور جملة كيميائية نحو حالة توازن',3),(13,'الوحدة 5',1),(14,'الوحدة 5',2),(15,'الوحدة 5: تطور جملة ميكانيكية',3),(16,'الوحدة 6',1),(17,'الوحدة 6',2),(18,'الوحدة 6: مراقبة تطور جملة كيميائية',3),(19,'الوحدة 7',1),(20,'الوحدة 7',2),(21,'الوحدة 7: التطورات المهتزة',3),(22,'الوحدة 8',1),(23,'الوحدة 8',2),(24,'الوحدة 8: مفهوم الموجة',3),(25,'الوحدة 9',1),(26,'الوحدة 9',2),(27,'الوحدة 10',1),(28,'الوحدة 10',2);
-/*!40000 ALTER TABLE `core_wihda` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `django_session`
---
-
-DROP TABLE IF EXISTS `django_session`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `django_session` (
-  `session_key` varchar(40) NOT NULL,
-  `session_data` longtext NOT NULL,
-  `expire_date` datetime(6) NOT NULL,
-  PRIMARY KEY (`session_key`),
-  KEY `django_session_expire_date_a5c62663` (`expire_date`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `django_session`
---
-
-LOCK TABLES `django_session` WRITE;
-/*!40000 ALTER TABLE `django_session` DISABLE KEYS */;
-INSERT INTO `django_session` VALUES ('w71c07lwp359ifka5l1v8e7yzp379kmx','OTQ1YWYxNWJhNDY1Y2Y3Y2ZhMTMzYTYxZDhmMDg5Nzc5YzNkNDJjNjp7Il9hdXRoX3VzZXJfaWQiOiIxIiwiX2F1dGhfdXNlcl9iYWNrZW5kIjoiZGphbmdvLmNvbnRyaWIuYXV0aC5iYWNrZW5kcy5Nb2RlbEJhY2tlbmQiLCJfYXV0aF91c2VyX2hhc2giOiJhYjkyYzUyNzdhODE1ZGQ2MmE4NGEzYzE0MTZhMzJjNzQzYmEzZTZkIn0=','2020-07-02 14:15:29.296570');
-/*!40000 ALTER TABLE `django_session` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users_user`
---
-
-DROP TABLE IF EXISTS `users_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users_user` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `password` varchar(128) NOT NULL,
-  `last_login` datetime(6) DEFAULT NULL,
-  `is_superuser` tinyint(1) NOT NULL,
-  `username` varchar(150) NOT NULL,
-  `first_name` varchar(30) NOT NULL,
-  `last_name` varchar(150) NOT NULL,
-  `email` varchar(254) NOT NULL,
-  `is_staff` tinyint(1) NOT NULL,
-  `is_active` tinyint(1) NOT NULL,
-  `date_joined` datetime(6) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users_user`
---
-
-LOCK TABLES `users_user` WRITE;
-/*!40000 ALTER TABLE `users_user` DISABLE KEYS */;
-INSERT INTO `users_user` VALUES (1,'pbkdf2_sha256$180000$bSiGmMbJRYMv$pvvdSmAUNPJELeffgDDclexBY5JM2XsgemgjtjOSviQ=','2020-06-18 14:15:29.047681',1,'bouchenaf','','','',1,1,'2020-06-18 14:14:49.811250');
-/*!40000 ALTER TABLE `users_user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users_user_groups`
---
-
-DROP TABLE IF EXISTS `users_user_groups`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users_user_groups` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `group_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `users_user_groups_user_id_group_id_b88eab82_uniq` (`user_id`,`group_id`),
-  KEY `users_user_groups_group_id_9afc8d0e_fk_auth_group_id` (`group_id`),
-  CONSTRAINT `users_user_groups_group_id_9afc8d0e_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`),
-  CONSTRAINT `users_user_groups_user_id_5f6f5a90_fk_users_user_id` FOREIGN KEY (`user_id`) REFERENCES `users_user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users_user_groups`
---
-
-LOCK TABLES `users_user_groups` WRITE;
-/*!40000 ALTER TABLE `users_user_groups` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users_user_groups` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users_user_user_permissions`
---
-
-DROP TABLE IF EXISTS `users_user_user_permissions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users_user_user_permissions` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `permission_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `users_user_user_permissions_user_id_permission_id_43338c45_uniq` (`user_id`,`permission_id`),
-  KEY `users_user_user_perm_permission_id_0b93982e_fk_auth_perm` (`permission_id`),
-  CONSTRAINT `users_user_user_perm_permission_id_0b93982e_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
-  CONSTRAINT `users_user_user_permissions_user_id_20aca447_fk_users_user_id` FOREIGN KEY (`user_id`) REFERENCES `users_user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users_user_user_permissions`
---
-
-LOCK TABLES `users_user_user_permissions` WRITE;
-/*!40000 ALTER TABLE `users_user_user_permissions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users_user_user_permissions` ENABLE KEYS */;
+LOCK TABLES `core_exercise` WRITE;
+/*!40000 ALTER TABLE `core_exercise` DISABLE KEYS */;
+INSERT INTO `core_exercise` VALUES (1,'https://drive.google.com/file/d/1i-UurVUCqIvSDjBn_8ME1yGm1TpVnChX/view?usp=sharing',3),(4,'https://drive.google.com/file/d/1GW-xKPLIb8RkJ72OEu8Yurd9TvmI11Q9/view?usp=sharing',3),(5,'https://drive.google.com/file/d/1gqYvIcZcp2fVTCtWm-88-dnmjjfIt_zU/view?usp=sharing',3);
+/*!40000 ALTER TABLE `core_exercise` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -271,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-07-04 21:43:21
+-- Dump completed on 2020-07-05 13:18:45

9b38e929ed05c688eb4fadf94c1f3a31f3ad98a7
rouizi <cinorouizi@gmail.com>
2020-07-06 13:36:02 +0200

    Changing the admin url

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from core.models import School, Wihda
+from core.models import School, Wihda, Exercise
 
 
 admin.site.register(School)
 admin.site.register(Wihda)
+admin.site.register(Exercise)
--- prof/urls.py
+++ prof/urls.py
@@ -3,11 +3,14 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include
+
+from decouple import config
+
 from core import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(config('ADMIN'), admin.site.urls),
     path('', views.index, name='index'),
     path('', include(('core.urls', 'core'), namespace='core'))
 ]

806f5c13beb8ba06707f103c0733ed5f4fa0301b
rouizi <cinorouizi@gmail.com>
2020-07-15 13:46:28 +0200

    Add summary_wihda model to the admin

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from core.models import School, Wihda, Exercise
+from core.models import School, Wihda, Exercise, Summary_wihda
 
 
 admin.site.register(School)
 admin.site.register(Wihda)
 admin.site.register(Exercise)
+admin.site.register(Summary_wihda)

ef6d65a21d0541620f8979979849df6627cac71e
rouizi <cinorouizi@gmail.com>
2020-07-20 15:32:02 +0200

    Add the order_name model to the admin

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from core.models import School, Wihda, Exercise, Summary_wihda
+from core.models import School, Wihda, Exercise, Summary_wihda, Order_name
 
 
 admin.site.register(School)
 admin.site.register(Wihda)
 admin.site.register(Exercise)
 admin.site.register(Summary_wihda)
+admin.site.register(Order_name)
